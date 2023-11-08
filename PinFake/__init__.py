00f0278160f995f88b7db68ce0713f5fa61ae153
Kauan Arthur <raidoka546@gmail.com>
2023-11-08 09:41:12 -0400

    mudança banco postgresql

--- PinFake/__init__.py
+++ PinFake/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)  #criar app flask com o nome da pasta atual
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "2720af2a2150f857eabd221fb77575c135eb953f"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
