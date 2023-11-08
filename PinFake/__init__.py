74695232e48adf0161c749c8d3d0de28f8a54ba5
Kauan Arthur <raidoka546@gmail.com>
2023-11-08 14:04:58 -0400

    .

--- PinFake/__init__.py
+++ PinFake/__init__.py
@@ -9,7 +9,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "2720af2a2150f857eabd221fb77575c135eb953f"
 app.config["UPLOAD_FOLDER"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "fotos_posts")
 
-database = SQLAlchemy(app)
+database = SQLAlchemy()
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "homepage"
