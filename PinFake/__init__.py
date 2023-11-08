fd4ba460cd65c2fa28a674cadad7179cc284fd46
Kauan Arthur <raidoka546@gmail.com>
2023-11-08 09:30:57 -0400

    tentativa 2

--- PinFake/__init__.py
+++ PinFake/__init__.py
@@ -6,7 +6,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)  #criar app flask com o nome da pasta atual
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "2720af2a2150f857eabd221fb77575c135eb953f"
-app.config["UPLOAD_FOLDER"] = "PinFake/static/fotos_posts"
+app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
